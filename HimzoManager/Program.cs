7eae8e53feea09700a92a1afe3b3ea5c3489801e
morzsiHUN <morzsiHUN@gmail.com>
2023-12-10 05:42:52 +0100

    Add project files.

---
+++ HimzoManager/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HimzoManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VboxTest vboxTest = new VboxTest();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ HimzoManager/VboxTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HimzoManager
+{
+    class VboxTest
+    {
+        private Vbox.vboxPortTypeClient vbox = new Vbox.vboxPortTypeClient();
+
+        public VboxTest()
+        {
+            Task<string> token = GetToken();
+            var machines = GetMachine(token.Result);
+            foreach (var item in machines.Result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(token.Result);
+        }
+
+        public async Task<string> GetToken()
+        {
+            var token = await vbox.IWebsessionManager_logonAsync("test", "test");
+            return token.returnval;
+        }
+
+        public async Task<string[]> GetMachine(string token)
+        {
+            var items = await vbox.IVirtualBox_getMachinesAsync(token);
+            return items.returnval;
+        }
+    }
+}
