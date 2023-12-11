f683f6adee1eba056448432e5898e4c1c05a384a
morzsiHUN <morzsiHUN@gmail.com>
2023-12-11 02:28:55 +0100

    snapshot fetures

---
+++ HimzoManager/VmResetSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HimzoManager
+{
+    class VmResetSettings
+    {
+        private string name = "";
+        private string id = "";
+        private DateTime startDate;
+        private DateTime resetDate;
+    }
+}
